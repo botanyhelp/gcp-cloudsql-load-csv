f97eaa72cf5eab3e273496b156c3c22bd6b2edeb
Thomas Maher <botanyhelp@gmail.com>
2024-08-27 23:22:27 +0000

    tested, works on real project, but streams too many records with emptyish filter, must change project_id to your project

--- click-analytics-gcp-20240827.py
+++ click-analytics-gcp-20240827.py
@@ -1,11 +1,16 @@
 from google.cloud import logging
+from datetime import datetime, timedelta, timezone
 
 def read_gcp_logs(project_id):
 
     """Reads logs from specified Google Cloud project"""
 
     logger = logging.Client(project=project_id)
-    filter_str = "timestamp >= \"now() - 1h\""
+    time_format = "%Y-%m-%dT%H:%M:%S.%f%z"
+    yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+    filter_str = (
+        f'timestamp>="{yesterday.strftime(time_format)}"'
+    )
 
     entries = logger.list_entries(filter_=filter_str)
 
@@ -13,5 +18,5 @@ def read_gcp_logs(project_id):
         print(entry)
 
 if __name__ == "__main__":
-    project_id = "PROJECT_ID"
+    project_id = "airy-berm-426714-j7"
     read_gcp_logs(project_id)
